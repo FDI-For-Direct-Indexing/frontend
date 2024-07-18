4369af27047987094217f29e01390ac39c11625e
kyeongseo90 <rudtj9099@gmail.com>
2024-07-18 09:14:38 +0900

    feat: router 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import {RouterProvider} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 function App() {
-  return <RouterProvider router={} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/router.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import ChatAi from "./chatai/chatAi";
+import Rank from "./rank/rank";
+import Dashboard from "./dashboard/dashboard";
+import Cart from "./cart/cart";
+
+const router = createBrowserRouter([
+  { path: "/", element: <ChatAi />, index: true },
+  { path: "/rank", element: <Rank />, index: true },
+  { path: "/dashboard", element: <Dashboard />, index: true },
+  { path: "/cart", element: <Cart />, index: true }
+]);
+
+export default router;
