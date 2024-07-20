e96ceb77d900b54b9c986c069640f3fdb55a6121
kyeongseo <rudtj9099@gmail.com>
2024-07-20 18:37:51 +0900

    💄design: 부트스트랩 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return <RouterProvider router={router} />;
