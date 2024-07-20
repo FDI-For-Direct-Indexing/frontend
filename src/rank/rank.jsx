f8734807852c3ae69f0e979dbd9b59a8d5879a19
kyeongseo <rudtj9099@gmail.com>
2024-07-20 18:41:47 +0900

    feat: rank 페이지 작성

--- src/rank/rank.jsx
+++ src/rank/rank.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { Header } from '../header/Header'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ChatSummary from './components/chatSummary';
+import CartSummary from './components/cartSummary';
+import Clustering from './components/clustering';
+import Parallel from './components/parallel';
+import RankList from './components/RankList';
 
 export default function Rank() {
   return (
-    <div>
-
-    </div>
+    <>
+      <Header/>
+      <Container>
+        <Row>
+          <Col>
+            <div>
+              <ChatSummary />
+              <CartSummary />
+            </div>
+          </Col>
+          <Col>
+            <div>
+              <RankList />
+              <div>
+                <Clustering />
+                <Parallel />
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
