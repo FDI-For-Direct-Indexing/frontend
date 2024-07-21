eb15584ac866cfdabb25e20b887abfe61e963175
kyeongseo <rudtj9099@gmail.com>
2024-07-22 01:15:47 +0900

    ✨feat: rank 페이지, 컴포넌트 추가

--- src/rank/components/weightgraph/titleline.jsx
+++ src/rank/components/weightgraph/titleline.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import "../../styles/titleline"
+// import React from "react";
+// import "../../styles/titleline.css"
 
-export default function TitleLine() {
-  return (
-    <div className="titleline-container">
-      <span className="titleline">
-        <span className="titleline-item">순위</span>
-        <span className="titleline-item group">군집</span>
-        <span className="titleline-item stock-code">종목코드</span>
-        <span className="titleline-item company-name">기업명</span>
-        <span className="titleline-item graph">그래프</span>
-      </span>
-      <hr className="separator-line" />
-    </div>
-  );
-}
+// export default function TitleLine() {
+//   return (
+//     <div className="titleline-container">
+//       <span className="titleline">
+//         <span className="titleline-item">순위</span>
+//         <span className="titleline-item group">군집</span>
+//         <span className="titleline-item stock-code">종목코드</span>
+//         <span className="titleline-item company-name">기업명</span>
+//         <span className="titleline-item graph">그래프</span>
+//       </span>
+//       <hr className="separator-line" />
+//     </div>
+//   );
+// }
--- src/rank/rank.jsx
+++ src/rank/rank.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavbarHeader } from '../header/navbar'
+import NavbarHeader from '../header/navbarHeader';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -8,30 +8,44 @@ import CartSummary from './components/cartSummary';
 import Clustering from './components/clustering';
 import Parallel from './components/parallel';
 import RankList from './components/weightgraph/rankList';
+import TitleLine from './components/weightgraph/titleLine';
+import CustomGraph from './components/weightgraph/customGraph';
+import { WeightProvider } from '../contexts/weightProvider';
+import { PlotProvider } from '../contexts/plotProvider';
 
 export default function Rank() {
   return (
-    <>
+    <div className='pageBack'>
       <NavbarHeader/>
-      <Container>
-        <Row>
-          <Col>
-            <div>
-              <ChatSummary />
-              <CartSummary />
-            </div>
-          </Col>
-          <Col>
-            <div>
-              <RankList />
-              <div>
-                <Clustering />
-                <Parallel />
-              </div>
-            </div>
-          </Col>
-        </Row>
+      
+      <Container style={{padding:'25px 100px'}} fluid >
+        <WeightProvider initialSliderValues={[30, 25, 15, 20, 10]}>
+          <PlotProvider>
+            <Row>
+              <Col xs={8}>
+                  <ChatSummary />
+              </Col>
+              <Col xs={4}>
+                  <CartSummary />
+              </Col>
+            </Row>
+            <Row>
+              {/* <Col>
+                <div>
+                  
+                  <CustomGraph title="순위" />
+                  <TitleLine />
+                  <RankList />
+                  <div>
+                    <Clustering />
+                    <Parallel />
+                  </div>
+                </div>
+              </Col> */}
+            </Row>
+          </PlotProvider>
+        </WeightProvider>
       </Container>
-    </>
+    </div>
   )
 }
