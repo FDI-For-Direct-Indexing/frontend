80e5aec49ea8191cf5817154c14ac3ec8a82f30f
kyeongseo <rudtj9099@gmail.com>
2024-07-22 01:14:09 +0900

    💄design: 전역 css 추가> 박스

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   return <RouterProvider router={router} />;
