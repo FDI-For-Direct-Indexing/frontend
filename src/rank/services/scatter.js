1cfe944b2ac02decb03453766556a6454a75f296
kyeongseo <rudtj9099@gmail.com>
2024-07-22 01:17:15 +0900

    ✨feat: 클러스터링 api 추가

---
+++ src/rank/services/scatter.js
@@ -0,0 +1,14 @@
+// import axios from "axios";
+// import { API_URL } from "../../common/api";
+
+// export const getClusterData = (stockList, sliderValues) => {
+//   return axios
+//     .post(`${API_URL.LOCAL}/api/cluster`, { stockList: stockList, sliderValues: sliderValues }) // 추후 데이터 변경
+//     .then((response) => {
+//       console.log("Get Clustered Data from Server : ", response.data);
+//       return response.data;
+//     })
+//     .catch((error) => {
+//       console.error("Error fetching cluster data:", error);
+//     });
+// };
