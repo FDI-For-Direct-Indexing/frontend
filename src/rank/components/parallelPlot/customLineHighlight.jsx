c3c62a1a25e016fc1a3f5bff592fe4200d822c4f
kyeongseo <rudtj9099@gmail.com>
2024-07-23 00:36:49 +0900

    ✨feat: parallel plot 컴포넌트

---
+++ src/rank/components/parallelPlot/customLineHighlight.jsx
@@ -0,0 +1,89 @@
+import React, { createElement, useCallback, useState } from "react";
+import { useTooltip } from "@nivo/tooltip";
+import { useSpring, animated } from "@react-spring/web";
+import { useAnimatedPath, useMotionConfig } from "@nivo/core";
+import { CustomTooltip } from "./customTooltip";
+
+const CustomLineHighlight = ({ computedData, variables, lineGenerator, highlightedGroup }) => {
+  const [activeData, setActiveData] = useState(-1);
+
+  return (
+    <g key="lines">
+      {computedData.map((datum) => (
+        <ParallelCoordinatesLine
+          key={`${"group" in datum ? datum.group.id : ""}${datum.id}`}
+          datum={datum}
+          variables={variables}
+          lineGenerator={lineGenerator}
+          lineWidth={activeData === -1 ? 1.5 : activeData === datum.id ? 5 : 1}
+          tooltip={(line) => <CustomTooltip line={line} />}
+          opacity={activeData === -1 ? 1 : activeData === datum.id ? 1 : 0.3}
+          onMouseEnter={(e) => setActiveData(datum.id) }
+          onMouseMove={(e) => setActiveData(datum.id) }
+          onMouseLeave={(e) => setActiveData(-1) }
+          // 강조된 그룹과 현재 그룹을 비교하여 강조 여부 결정
+          highlight={ highlightedGroup === null || highlightedGroup === datum.group.id }
+        />
+      ))}
+    </g>
+  );
+};
+
+export const ParallelCoordinatesLine = ({
+  datum,
+  variables,
+  lineGenerator,
+  lineWidth,
+  opacity,
+  tooltip,
+  testIdPrefix,
+  onMouseEnter,
+  onMouseMove,
+  onMouseLeave,
+  highlight, // 강조 여부 추가
+}) => {
+  const { showTooltipFromEvent, hideTooltip } = useTooltip();
+  const handleMouseHover = useCallback(
+    (event) => {
+      showTooltipFromEvent(createElement(tooltip, { datum, variables }), event);
+    },
+    [showTooltipFromEvent, datum, variables]
+  );
+
+  const { animate, config: springConfig } = useMotionConfig();
+  const animatedPath = useAnimatedPath(lineGenerator(datum.points));
+  const animatedProps = useSpring({
+    color: datum.color,
+    opacity: highlight ? opacity : 0.1, // 강조 여부에 따른 투명도 설정
+    config: springConfig,
+    immediate: !animate,
+  });
+
+  return (
+    <animated.path
+      d={animatedPath}
+      stroke={animatedProps.color}
+      strokeWidth={highlight ? lineWidth : 1} // 강조 여부에 따른 너비 설정
+      strokeLinecap="round"
+      opacity={animatedProps.opacity}
+      fill="none"
+      onMouseEnter={(e) => {
+        onMouseEnter(e);
+        handleMouseHover(e);
+      }}
+      onMouseMove={(e) => {
+        onMouseMove(e);
+        handleMouseHover(e);
+      }}
+      onMouseLeave={(e) => {
+        onMouseLeave(e);
+        hideTooltip(e);
+      }}
+      data-test-id={
+        testIdPrefix ? `${testIdPrefix}.line_${datum.id}` : undefined
+      }
+    />
+  );
+};
+
+export default CustomLineHighlight;
---
+++ src/rank/components/parallelPlot/parallelCoordinate.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { ResponsiveParallelCoordinates } from "@nivo/parallel-coordinates";
+import { CLUSTER } from "../../../constants/color";
+import { CustomTooltip } from "./customTooltip";
+import CustomLineHighlight from "./customLineHighlight";
+
+const handleLegendClick = (legend, highlightedGroup, setHighlightedGroup) => {
+  if (highlightedGroup === legend.data.id) {
+    setHighlightedGroup(null); // 강조된 상태라면 해제
+  } else {
+    setHighlightedGroup(legend.data.id); // 강조되지 않은 상태라면 강조
+  }
+};
+
+export const ParallelCoordinate = ({ data }) => {
+  const [highlightedGroup, setHighlightedGroup] = useState(null); // 상태 추가
+
+  return (
+    data && (
+      <ResponsiveParallelCoordinates
+        isInteractive
+        data={data}
+        variables={[
+          { 
+            group: 0, id: "수익성", value: "수익성",  ticksPosition: "after",
+            legendPosition: "start", legendOffset: 30, min: 0, max: 100,
+          },
+          {
+            group: 1, id: "안정성", value: "안정성", ticksPosition: "after",
+            legendPosition: "start", legendOffset: -10, min: 0, max: 100,
+          },
+          {
+            group: 2, id: "활동성", value: "활동성", ticksPosition: "after",
+            legendPosition: "start", legendOffset: -10, min: 0, max: 100,
+          },
+          {
+            group: 3, id: "생산성", value: "생산성", ticksPosition: "after",
+            legendPosition: "start", legendOffset: -10, min: 0, max: 100,
+          },
+          {
+            group: 4, id: "오공지수", value: "오공지수", ticksPosition: "after", 
+            legendPosition: "start", legendOffset: -10, min: 0, max: 100,
+          }, 
+        ]}
+        groupBy="group"
+        margin={{ top: 5, right: 25, bottom: 23, left: 0 }}
+        curve="linear"
+        colors={CLUSTER}
+        colorBy="group"
+        lineWidth={3}
+        lineOpacity={0.5}
+        pixelRatio={1.25}
+        layers={[
+          "axes",
+          "legends",
+          (props) => (
+            <CustomLineHighlight
+              {...props}
+              highlightedGroup={highlightedGroup}
+            />
+          ),
+          "mesh",
+          "annotations",
+        ]}
+        tooltip={(line) => <CustomTooltip line={line} />}
+        legends={[
+          {
+            anchor: "bottom",
+            direction: "row",
+            justify: false,
+            translateX: 29,
+            translateY: 36,
+            itemsSpacing: 2,
+            itemWidth: 68,
+            itemHeight: 40,
+            itemDirection: "left-to-right",
+            itemOpacity: 0.85,
+            symbolSize: 13,
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemOpacity: 1,
+                  itemTextColor: "#000",
+                },
+              },
+            ],
+            itemTextColor: "#999",
+            symbolShape: "circle",
+            onClick: (legend) =>
+              handleLegendClick(legend, highlightedGroup, setHighlightedGroup),
+          },
+        ]}
+      />
+    )
+  );
+};
