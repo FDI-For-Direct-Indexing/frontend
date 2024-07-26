b08cd44b3638505d7d47b6c92631aa51e2e56089
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-07-26 20:03:09 +0900

    ✨ feat: 섹터칩 컴포넌트 추가

---
+++ src/common/ui/sectionChip.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { colorMapping } from "../../constants/color";
+
+export default function SectionChip({ sector }) {
+  const { color, font } = colorMapping[sector] || { chip: "#000000", font: "#ffffff" }; // 기본 색상 설정
+
+  return (
+    <div
+      style={{
+        width: "fit-content",
+        backgroundColor: `${color}`,
+        color: `${font}`,
+        borderRadius: "9px",
+        padding: "0 5px",
+        textAlign: "center",
+      }}
+    >
+      {sector}
+    </div>
+  );
+}
