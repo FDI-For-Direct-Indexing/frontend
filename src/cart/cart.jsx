c2e007e581156686f0a0a3606d13fff64dad0ed2
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-07-26 20:04:18 +0900

    ✨ feat: 장바구니 페이지 구현(메인 파일)

--- src/cart/cart.jsx
+++ src/cart/cart.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import NavbarHeader from "../header/navbarHeader";
+import "./styles/cart.css";
+import DiList from "./components/diList";
+import { useParams } from "react-router-dom";
 
 export default function Cart() {
+  const { userId } = useParams();
+
   return (
-    <div>
-      
+    <div className="cart-component">
+      <NavbarHeader />
+      <div className="cart-container">
+        <p className="cart-title">다이렉트 인덱싱 장바구니</p>
+        <div className="cart-list">
+          <div>
+            <DiList userId={userId} />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
