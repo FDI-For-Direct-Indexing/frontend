f32aa0f6eed5ffbfb9e8c03f7bff17f143daddc0
yoonjaeuk <jasper09@ajou.ac.kr>
2024-07-27 00:59:03 +0900

    Feat.채팅방 구현

--- src/rank/rank.jsx
+++ src/rank/rank.jsx
@@ -1,31 +1,30 @@
-import React, { useState } from 'react'
-import NavbarHeader from '../header/navbarHeader';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import ChatSummary from './components/chatSummary';
-import CartSummary from './components/cartSummary';
-import Clustering from './components/clustering';
-import Parallel from './components/parallelPlot/parallel';
-import RankList from './components/weightgraph/rankList';
-import TitleLine from './components/weightgraph/titleLine';
-import CustomGraph from './components/weightgraph/customGraph';
-import { WeightProvider } from '../contexts/weightProvider';
-import { PlotProvider } from '../contexts/plotProvider';
-import DashedChart from './components/parallelPlot/dashedChart';
+import React, { useState } from "react";
+import NavbarHeader from "../header/navbarHeader";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ChatSummary from "./components/chatSummary";
+import CartSummary from "./components/cartSummary";
+import Clustering from "./components/clustering";
+import Parallel from "./components/parallelPlot/parallel";
+import RankList from "./components/weightgraph/rankList";
+import TitleLine from "./components/weightgraph/titleLine";
+import CustomGraph from "./components/weightgraph/customGraph";
+import { WeightProvider } from "../contexts/weightProvider";
+import { PlotProvider } from "../contexts/plotProvider";
+import DashedChart from "./components/parallelPlot/dashedChart";
 
 export default function Rank() {
-
   const [highlightGroupIdx, setHighlightGroupIdx] = useState(null);
 
   return (
-    <div className='pageBack'>
+    <div className="pageBack">
       <NavbarHeader />
 
-      <Container style={{ padding: '25px 5vw' }} fluid >
+      <Container style={{ padding: "25px 5vw" }} fluid>
         <WeightProvider initialSliderValues={[30, 25, 15, 20, 10]}>
           <PlotProvider>
-            <Row style={{ marginBottom: '25px' }}>
+            <Row style={{ marginBottom: "25px" }}>
               <Col xs={8}>
                 <ChatSummary />
               </Col>
@@ -35,7 +34,7 @@ export default function Rank() {
             </Row>
             <Row>
               <Col xs={6}>
-                <div className='basicBox' style={{ height: '750px' }}>
+                <div className="basicBox" style={{ height: "750px" }}>
                   <CustomGraph title="맞춤형 순위" />
                   <TitleLine />
                   <RankList setGroupIdx={setHighlightGroupIdx} />
@@ -51,5 +50,5 @@ export default function Rank() {
         </WeightProvider>
       </Container>
     </div>
-  )
+  );
 }
