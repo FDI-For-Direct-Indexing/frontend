50909e00df32f890f9e4159c65dcac7bf2d6aaed
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-07-27 16:23:12 +0900

    ✨ feat: 최신 뉴스 크롤링 css 컴포넌트 분리

--- src/dashboard/components/relatedNews.jsx
+++ src/dashboard/components/relatedNews.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import questionmark from "../../assets/image/questionmark.svg";
 import GetNews from "../services/newsCrawling";
-import { Table } from "react-bootstrap";
+import "../styles/relatedNews.css";
 
 export default function RelatedNews({ code }) {
   const [newsList, setNewsList] = useState([]);
@@ -15,7 +15,6 @@ export default function RelatedNews({ code }) {
         const response = await axios.get(`http://localhost:4000/api/stocksDetail/${code}`);
         const stockName = response.data.name;
         const news = await GetNews(stockName);
-        console.log(news);
         setNewsList(news);
       } catch (error) {
         console.error("Error fetching data", error);
@@ -27,36 +26,33 @@ export default function RelatedNews({ code }) {
 
   return (
     <div className="related-news">
-      <div>
-        <div>
-          <p className="news-title">관련 기사</p>
-          <p className="news-info">{today} 기준 집계된 데이터입니다.</p>
-        </div>
-        <img className="question-mark" src={questionmark} alt="questionmark" />
+      <div className="related-news-header">
+        <p className="related-news-title">관련 기사</p>
+        <p className="news-info">{today} 기준 집계된 데이터입니다.</p>
       </div>
       <div className="news-list">
-        <Table striped bordered hover>
+        <table>
           <thead>
             <tr>
-              <th>신문사</th>
-              <th>기사 제목</th>
-              <th>날짜</th>
+              <th className="news-company">신문사</th>
+              <th className="news-title">기사 제목</th>
+              <th className="news-date">날짜</th>
             </tr>
           </thead>
           <tbody>
             {newsList.map((news) => (
               <tr key={news.title}>
                 <td>{news.press}</td>
-                <td>
-                  <a href={news.link} target="_blank" rel="noreferrer">
+                <td className="news-title-content">
+                  <a href={news.url} target="_blank" rel="noreferrer">
                     {news.title}
                   </a>
                 </td>
-                <td>{news.date}</td>
+                <td className="news-date-content">{news.date}</td>
               </tr>
             ))}
           </tbody>
-        </Table>
+        </table>
       </div>
     </div>
   );
