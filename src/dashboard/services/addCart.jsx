63f3a06ab35d4cbd44d5163dcb3cb43ebcc5995c
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-07-30 22:25:37 +0900

    ✨ feat: 장바구니 추가 api 연동

--- src/dashboard/services/addCart.jsx
+++ src/dashboard/services/addCart.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export default async function AddCart({ code, userId }) {
   try {
-    console.log("Adding to cart with code:", code, "and userId:", userId);
     const response = await axios.post("http://localhost:4000/api/cart", {
       userId,
       code,
