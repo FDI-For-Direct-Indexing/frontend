f3d588e0a23f2c72e16dd4185b088fec41ae6bff
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-07-30 22:24:02 +0900

    ✨ feat: 주식 상세 페이지에서 장바구니 삭제/추가 구현

--- src/dashboard/components/clickCart.jsx
+++ src/dashboard/components/clickCart.jsx
@@ -1,19 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import cart from "../../assets/image/cart.svg";
 import fullcart from "../../assets/image/full-cart.svg";
 import AddCart from "../services/addCart";
+import DeleteCart from "../services/deleteCart";
 import { Modal, Button } from "react-bootstrap";
 
-export default function ClickCart({ code, name, userId }) {
-  const [isCartFull, setIsCartFull] = useState(false);
+export default function ClickCart({ code, name, userId, isCartFull, setIsCartFull }) {
   const [showModal, setShowModal] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
 
   const handleCartClick = async () => {
-    await AddCart({ code, userId });
     if (isCartFull) {
+      await DeleteCart({ code, userId });
       setModalMessage(`${name}이 장바구니에서 삭제되었습니다.`);
     } else {
+      await AddCart({ code, userId });
       setModalMessage(`${name}이 장바구니에 추가되었습니다.`);
     }
     setIsCartFull((prev) => !prev);
