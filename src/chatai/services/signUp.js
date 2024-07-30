e120e507b283ed105069481507e78bbca4cc857b
kyeongseo <rudtj9099@gmail.com>
2024-07-31 01:52:56 +0900

    fix: api 오타 수정

--- src/chatai/services/signUp.js
+++ src/chatai/services/signUp.js
@@ -3,7 +3,7 @@ import { API_URL } from "../../common/api";
 
 export const signUp = (nickname) => {
   return axios
-    .post(`${API_URL.LOCAL}/api/user`, { name: nickname })
+    .post(`${API_URL.LOCAL}/api/users`, { name: nickname })
     .then((response) => {
       console.log(response.data);
       return response.data._id;
