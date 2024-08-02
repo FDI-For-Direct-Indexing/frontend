41da293c05ee1292628c78d2370537ce6c80a31b
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-08-02 20:06:19 +0900

    ✨ feat: 배치 작업 시, 서비스 이용 불가 메시지 출력하는 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import MaintenanceMessage from "./batchMaintenance";
+import Start from "./chatai/start";
+import ChatAi from "./chatai/chatAi";
+import Rank from "./rank/rank";
+import Dashboard from "./dashboard/dashboard";
+import Cart from "./cart/cart";
+import ProgressPage from "./common/ui/progresspage";
+import Backtest from "./backtest/backtest";
+import MaintenancePage from "./maintenance";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Router>
+      <MaintenanceMessage>
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route path="/chat/:userId" element={<ChatAi />} />
+          <Route path="/chat" element={<Navigate to="/" replace />} />
+          <Route path="/rank/:userId" element={<Rank />} />
+          <Route path="/rank" element={<Navigate to="/" replace />} />
+          <Route path="/dashboard/:userId/:code" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Navigate to="/" replace />} />
+          <Route path="/cart/:userId" element={<Cart />} />
+          <Route path="/cart" element={<Navigate to="/" replace />} />
+          <Route path="/loading" element={<ProgressPage />} />
+          <Route path="/backtest/:userId" element={<Backtest />} />
+          <Route path="/maintenance" element={<MaintenancePage />} />
+        </Routes>
+      </MaintenanceMessage>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/maintenance.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MaintenancePage = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20%" }}>
+      <h1>현재가 및 종가 배치 작업 시간입니다. 잠시 후 다시 시도해주세요.</h1>
+      <h1>(평일 09:00 - 09:03, 15:20 - 15:35)</h1>
+    </div>
+  );
+};
+
+export default MaintenancePage;
