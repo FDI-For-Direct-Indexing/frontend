8b8068c94b88a3f82a22d556d00bfc6a296adafc
yoonjaeuk <jasper09@ajou.ac.kr>
2024-08-04 19:55:14 +0900

    Fix.챗화면 initial message 안나오는 오류해결

--- src/common/ui/progresspage.jsx
+++ src/common/ui/progresspage.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
-import { GridLoader } from 'react-spinners'
-import {PROGRESS_ICON} from '../../constants/color'
+import React from "react";
+import { GridLoader } from "react-spinners";
+import { PROGRESS_ICON } from "../../constants/color";
 
-export default function ProgressPage({name}) {
+export default function ProgressPage({ name }) {
   return (
-    <div style={{height:'100vh', display:"flex", alignItems:"center", justifyContent:'center', backgroundColor:'#F9F9F9'}}>
-      <div style={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:'space-evenly',
-        height: '40vh', width: '50vw', maxWidth: '600px',
-        padding:'30px', border:'1px solid #E0E0E0', borderRadius:'10px', backgroundColor:'white', boxShadow:'0px 18px 18px rgba(0, 0, 0, 0.25)'
-      }}>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#F9F9F9",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+          height: "40vh",
+          width: "50vw",
+          maxWidth: "600px",
+          padding: "30px",
+          border: "1px solid #E0E0E0",
+          borderRadius: "10px",
+          backgroundColor: "white",
+          boxShadow: "0px 18px 18px rgba(0, 0, 0, 0.25)",
+        }}
+      >
         <GridLoader size={30} speedMultiplier={0.5} color={PROGRESS_ICON} />
-        <div style={{display:'flex', alignItems:'center', flexDirection:'column'}}>
-          <p style={{fontFamily:'SpoqaHanSansNeo-Medium', fontSize:19}}>{name}님에게 맞는 종목을 찾아볼게요.</p>
-          <p style={{fontFamily:'SpoqaHanSansNeo-Medium', fontSize:19}}>조금만 기다려주세요!</p>
+        <div style={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
+          <p style={{ fontFamily: "SpoqaHanSansNeo-Medium", fontSize: 19 }}>
+            고객님에게 맞는 종목을 찾아볼게요.
+          </p>
+          <p style={{ fontFamily: "SpoqaHanSansNeo-Medium", fontSize: 19 }}>조금만 기다려주세요!</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/rank/components/chatSummary.jsx
+++ src/rank/components/chatSummary.jsx
@@ -1,12 +1,11 @@
-import React, { useContext, useEffect, useState } from 'react';
-import '../styles/chatsummary.css';
-import CHAT from '../../assets/image/chat-symbol.png';
-import { CHAT_SAY } from '../../constants/color';
-import { WeightContext } from '../../contexts/weightProvider';
-import { getIndicatorsResult } from '../services/indicators';
+import React, { useContext, useEffect, useState } from "react";
+import "../styles/chatsummary.css";
+import CHAT from "../../assets/image/chat-symbol.png";
+import { CHAT_SAY } from "../../constants/color";
+import { WeightContext } from "../../contexts/weightProvider";
+import { getIndicatorsResult } from "../services/indicators";
 
 export default function ChatSummary({ userId, name }) {
-
   const { sliderValues, setSliderValues } = useContext(WeightContext);
   const rates = ["안정성", "수익성", "활동성", "성장성", "언급량", "감정지수"];
   const [maxPreference, setMaxPreference] = useState("");
@@ -21,25 +20,31 @@ export default function ChatSummary({ userId, name }) {
       valuesToRates.push(values.활동성);
       valuesToRates.push(values.언급량);
       valuesToRates.push(values.감성지수);
+      console.log(valuesToRates, "벨류");
       setSliderValues(valuesToRates);
+      console.log(sliderValues);
       const maxIdx = valuesToRates.reduce((maxIndex, currentValue, currentIndex, array) => {
         return currentValue > array[maxIndex] ? currentIndex : maxIndex;
       }, 0);
       setMaxPreference(rates[maxIdx]);
-    }
+      console.log(maxPreference, "최대");
+    };
     fetchData();
   }, []);
 
   return (
-    <div className='chatSummaryBallon'>
+    <div className="chatSummaryBallon">
       <img src={CHAT} alt="chat" width="23" />
-      <div style={{ marginLeft: '10px' }}>
-        <p style={{ color: CHAT_SAY, marginBottom: '10px' }}>{name}님에게 적합한 주식 종목 순위를 보여드릴게요. </p>
+      <div style={{ marginLeft: "10px" }}>
+        <p style={{ color: CHAT_SAY, marginBottom: "10px" }}>
+          {name}님에게 적합한 주식 종목 순위를 보여드릴게요.{" "}
+        </p>
         <p style={{ color: CHAT_SAY }}>
-          대화 결과, {name}님은 {maxPreference}이 높은 주식을 선호하는 것 같아요. 만약 다른 결과를 보고싶다면 아래의 슬라이더를 조절하거나 다시 대화를 시작해보세요.
-          종목을 클릭하면 자세한 정보를 볼 수 있고, 다이렉트 인덱싱을 위해 장바구니에 담을 수 있어요.
+          대화 결과, {name}님은 {maxPreference}이 높은 주식을 선호하는 것 같아요. 만약 다른 결과를
+          보고싶다면 아래의 슬라이더를 조절하거나 다시 대화를 시작해보세요. 종목을 클릭하면 자세한
+          정보를 볼 수 있고, 다이렉트 인덱싱을 위해 장바구니에 담을 수 있어요.
         </p>
       </div>
     </div>
-  )
+  );
 }
